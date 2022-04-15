bebf6c0c3fc26d34e3502b4822625fca34e6c1e6
FroilanRoy <58676587+FroilanRoy@users.noreply.github.com>
2022-04-15 08:26:26 -0700

    Add files via upload

---
+++ ProblemThree.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "_pause.h"
+
+using namespace std;
+
+int main() {
+ 
+    cout << "Hello" << endl;
+    cout << "W";
+    cout << "o";
+    cout << "r";
+    cout << "l";
+    cout << "d" << endl;
+
+    system ("pause");
+    return 0;
+}
---
+++ ProblemTwo.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "_pause.h"
+
+using namespace std;
+
+int main() {
+
+    cout << "H";
+    cout << "E";
+    cout << "L";
+    cout << "L";
+    cout << "O" << endl;
+    cout << "W";
+    cout << "O";
+    cout << "R";
+    cout << "L";
+    cout << "D" << endl;
+
+    system ("pause");
+    return 0;
+}
